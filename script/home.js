c53ebfb60c837c70af784841cac7b7b271d50f8f
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-11-10 11:07:07 +0100

    style home fatta, inizio js

---
+++ script/home.js
@@ -0,0 +1,6 @@
+fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZWI1MDI1NGU4ODAwMTgzZjE4NzYiLCJpYXQiOjE2OTk2MDUzMjgsImV4cCI6MTcwMDgxNDkyOH0.rgBYaTD3P8KZnyEbfr9HOlOHnFuf_5vog-7QdGwv8zg",
+  },
+});
